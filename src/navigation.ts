9811e1980f7e566fd5d01416f053d4286ae70a3a
DemianMArin <demianmarin03@gmail.com>
2025-06-25 22:37:49 -0600

    First commit

---
+++ src/navigation.ts
@@ -0,0 +1,15 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'About me',
+      href: '/portfolio_pages/aboutme',
+    },
+    {
+      text: 'CV',
+      href: '/portfolio_pages/cv',
+    },
+  ],
+};
+
