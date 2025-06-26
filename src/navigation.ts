1fb32b4acda17ab07938168e48b780348153498c
DemianMArin <demianmarin03@gmail.com>
2025-06-26 12:42:36 -0600

    Added permalinks, missing 404

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,14 +1,14 @@
-// import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
     {
       text: 'About me',
-      href: '/portfolio_pages/aboutme',
+      href: getPermalink('/portfolio_pages/aboutme'),
     },
     {
       text: 'CV',
-      href: '/portfolio_pages/cv',
+      href: getPermalink('/portfolio_pages/cv'),
     },
   ],
 };
